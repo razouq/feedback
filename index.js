6ca477c0676a56e515c8d45d53268b7768fe6940
anass bendarsi <bendarsi@gmail.com>
2020-12-09 19:03:20 +0100

    test deployment

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ app.get('/', (req, res) => {
   res.send({hi: 'there'});
 });
 
+app.get('/new', (req, res) => {
+  res.send({new: 'new'});
+});
+
 const PORT = process.env.PORT ||5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
