13a6f70ef94b5ebf18211d5cac4834ccebf68d5b
anass bendarsi <bendarsi@gmail.com>
2020-12-09 23:06:44 +0100

    google oauth

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hi: 'there'});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      // try to not use it
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log(refreshToken);
+      console.log(profile);
+    }
+  )
+);
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email'],
+}));
+
+// ?code=... is added to the URI, So google strategy will handle the requeste differently
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
 });
 
-app.get('/new', (req, res) => {
-  res.send({new: 'new'});
-});
 
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
