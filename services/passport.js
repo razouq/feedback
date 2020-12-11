ddbc0131246957715c116b2f4556c41a9a7ea0ab
anass bendarsi <bendarsi@gmail.com>
2020-12-11 23:29:07 +0100

    fix: create unique user

--- services/passport.js
+++ services/passport.js
@@ -13,9 +13,14 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({
-        googleId: profile.id,
-      }).save();
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+        } else {
+          new User({
+            googleId: profile.id,
+          }).save();
+        }
+      });
     }
   )
 );
