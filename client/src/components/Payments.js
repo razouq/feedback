766ffcd3586530a9805fbe1cea8df5595938f9c9
anass bendarsi <bendarsi@gmail.com>
2020-12-27 15:32:31 +0100

    update user credits after payment

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import {useDispatch} from 'react-redux';
+import {handleToken} from '../actions';
 
 const Payments = () => {
+  const dispatch = useDispatch();
+
   return (
     <StripeCheckout
       name="Feedback"
@@ -9,7 +13,7 @@ const Payments = () => {
       // 5$
       amount={500}
       // the token that we get back from stripe
-      token={token => console.log(token)}
+      token={token => dispatch(handleToken(token))}
       stripeKey={process.env.REACT_APP_STRIPE_KEY}
     >
       <button className="btn">Add Credits</button>
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require("./config/keys");
+const bodyParser = require('body-parser');
 
 require("./models/User");
 require("./services/passport");
@@ -16,8 +17,11 @@ mongoose.connect(
 );
 
 const authRoutes = require("./routes/authRoutes");
+const billingRoutes = require("./routes/billingRoutes");
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -30,6 +34,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 authRoutes(app);
+billingRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
