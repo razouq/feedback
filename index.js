094b5f234b7ad2a705609ae97cca497dc834b0ae
anass bendarsi <bendarsi@gmail.com>
2020-12-28 09:42:56 +0100

    deployment

--- index.js
+++ index.js
@@ -36,5 +36,16 @@ app.use(passport.session());
 authRoutes(app);
 billingRoutes(app);
 
+if(process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets like main.js and main.css
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html if it does not recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
