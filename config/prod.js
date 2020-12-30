488ee18ac38a5c341e00ee14eb347e5eb5c8d9cb
anass bendarsi <bendarsi@gmail.com>
2020-12-30 21:25:08 +0100

    fix dep

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,20 @@
 // production keys
+// module.exports = {
+//   googleClientID: process.env.GOOGLE_CLIENT_ID,
+//   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   mongoURI: process.env.MONGO_URI,
+//   cookieKey: process.env.COOKIE_KEY,
+//   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+//   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+// };
+
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  googleClientID: '302621985895-q1c29t9oc5f7j2fhh532bubuha4vuqbg.apps.googleusercontent.com',
+  googleClientSecret: 'Cn459txcpz9D3TuryNLsg7xH',
+  mongoURI: 'mongodb://localhost:27017/feedback',
+  cookieKey: 'azenfoziefnazefn',
+  stripePublishableKey:
+    "pk_test_51I2wqlJtSPYc9UqxBPEo6voV0xp85AnhyQfQzoTl2A6zRMx9rUR80vRzhVAkTrQf7uUUHvZxpciQJXaeghMFzSlG00cUb99jRL",
+  stripeSecretKey:
+    "sk_test_51I2wqlJtSPYc9UqxqHWmBZaQpAjYMpu4j9F9NeV3W30rwnp05jKCP9lGSy9IE1W5UGEEp1wcUBum2x3QiBK5XmW300JE2sl6t3",
 };
