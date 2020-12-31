eeed0ec12550e3bd7a1f1d72863a1912e4f6f7e8
anass bendarsi <bendarsi@gmail.com>
2020-12-31 21:46:57 +0100

    fix env vars

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ app.use(passport.session());
 authRoutes(app);
 billingRoutes(app);
 
+app.get('/ping', (req, res) => {
+  res.json(process.env.PONG_ANSWER);
+});
+
 if(process.env.NODE_ENV === 'production') {
   // Express will serve up production assets like main.js and main.css
   app.use(express.static('client/build'));
